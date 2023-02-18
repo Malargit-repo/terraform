b3743d7de9ab91624abf394109fd2b5cef437aaf
Malargit-repo <125748016+Malargit-repo@users.noreply.github.com>
2023-02-18 20:08:22 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "access_key"{
+default = "AKIAREO36Y2KBLW4ZA4M"
+}
+
+variable "secret_key"{
+default = "3yWz3kVaZsLn9zbYxFwmqSv2WpXra931GaVMfDTh"
+}
+
+variable "region"{
+default = "us-east-2"
+}
